b90d962d05e4c4dc743794c194dd993547fbd7aa
TechBaba <Tech.Baba@yahoo.com>
2023-01-14 11:46:48 +0530

    Commit 3

--- src/test/java/com/gradledemo/SayHelloTest.java
+++ src/test/java/com/gradledemo/SayHelloTest.java
@@ -8,6 +8,8 @@ public class SayHelloTest {
     @Test
     public void testSayHello() throws IOException {
         SayHello.main(new String[]{"en"});
+        System.out.println("done some changes, done local changes to push ");
+        System.out.println("done more changes");
     }
 
 }
